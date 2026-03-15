9681d19153b2dd91e748351656c2a90d302dc84e
Dawn-o <ruselaq@gmail.com>
2026-03-16 06:54:25 +0800

    Revert "fix: optimize image config for Vercel free tier"
    This reverts commit 441db78d247907ab887c7d0be15ee830faa4dbfe.

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,8 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
   images: {
-    unoptimized: process.env.VERCEL_ENV === "production" ? false : true,
-    formats: ["image/webp"],
-    qualities: [75],
+    formats: ["image/webp", "image/avif"],
+    qualities: [75, 80, 85],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
